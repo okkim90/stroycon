261fc8e15d7bb589050129144b34ece4cec942cb
DESKTOP-RR4JT6L\USER <okkim@donga.com>
2022-03-17 17:47:40 +0900

    asdfasdf

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(function(){
+    var $footer_policy = $('.footer_policy');
+    $('.btn_policy').on('click',function(){
+        if($footer_policy.hasClass('on')){
+            $footer_policy.removeClass('on');
+            $footer_policy.find('.txt_box').slideUp(200);
+        }else{
+            $footer_policy.addClass('on');
+            $footer_policy.find('.txt_box').slideDown(200);
+        }
+    });
+});
